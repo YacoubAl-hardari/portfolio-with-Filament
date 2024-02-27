b21cce52a46bfd76bbcd6576e8f55ce865010667
YacoubAl-hardari <yacoubhaidari801@gmail.com>
2024-02-27 20:19:51 +0300

    ..

--- app/Livewire/Front/Home/HeroSection/ShowHero.php
+++ app/Livewire/Front/Home/HeroSection/ShowHero.php
@@ -11,7 +11,7 @@ class ShowHero extends Component
     public function render()
     {
         $Hero = Hero::latest()->first();
-        $data['SoicalMedais'] = SoicalMedai::latest()->first();
+        $SoicalMedais= SoicalMedai::latest()->first();
         return view('livewire.front.home.hero-section.show-hero',compact('Hero','SoicalMedais'));
     }
 }
